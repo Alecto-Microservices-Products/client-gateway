5e1fd6c03549242c9ab8ed9854e523a0fe13519b
Harry Ore Lara <harryorelara@gmail.com>
2024-05-06 15:32:38 -0500

    Migrando a NATS

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,26 +1,22 @@
 import { Module } from '@nestjs/common';
 import { ProductsController } from './products.controller';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import { PRODUCT_SERVICE, envs } from 'src/config';
+import { NatsModule } from 'src/transports/nats.module';
 
 @Module({
   controllers: [ProductsController],
   providers: [],
   imports: [
-    ClientsModule.register([
-      {
-        name: PRODUCT_SERVICE, 
-        transport: Transport.TCP,
-        options:{
-          host:envs.productsMicroserviceHost,
-          port:envs.productsMicroservicePort
-        }
-      }
-    ])
+    NatsModule
+    // ClientsModule.register([
+    //   {
+    //     name: NATS_SERVICE, 
+    //     transport: Transport.NATS,
+    //     options:{
+    //       servers: envs.natsServers
+    //     }
+    //   }
+    // ])
   ]
 })
 export class ProductsModule {
-  constructor(){
-    console.log(envs);
-  }
 }
